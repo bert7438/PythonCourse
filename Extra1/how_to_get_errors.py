237bc674b4c40359c0d781559d171b6fa4ce8b80
Albert <bert7438@gmail.com>
2021-02-20 23:46:43 +0300

    added errors for Extra1

---
+++ Extra1/how_to_get_errors.py
@@ -0,0 +1,33 @@
+import math
+
+# def cannot_assign():
+#    1 = w
+
+
+# def invalid_syntax():
+#    1kla$ = 1
+
+
+# def not_defined():
+#    print(a)
+
+
+# def unsupported_type():
+#    s = "Hello"
+#    print(s // 2)
+
+
+# def indent():
+# return 0
+
+
+# def zero_div():
+#    return 1 / 0
+
+
+# def math_domain():
+#    return math.sqrt(-1)
+
+
+# def overflow():
+#    print(1 - math.exp(-4 * 1000000 * -0.0641515994108))
